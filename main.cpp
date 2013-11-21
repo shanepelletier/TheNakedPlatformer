4aa325c239a1a722c3afb9598e630c3b12099db5
Shane M. Pelletier <lotios611@gmail.com>
2013-11-20 22:10:40 -0400

    Optimize surface loading

--- main.cpp
+++ main.cpp
@@ -2,16 +2,50 @@
 #include <SDL/SDL.h>
 #include <SDL/SDL_image.h>
 
+SDL_Surface* loadImage(const char* filename)
+{
+	SDL_Surface* loadedImage = NULL;
+	SDL_Surface* optimizedImage = NULL;
+	loadedImage = IMG_Load(filename);
+	if (loadedImage != NULL)
+	{
+		optimizedImage = SDL_DisplayFormat(loadedImage);
+		SDL_FreeSurface(loadedImage);
+	}
+	return optimizedImage;
+}
+
+void applySurface(int x, int y, SDL_Surface* source, SDL_Surface* destination)
+{
+	SDL_Rect offset;
+	offset.x = x;
+	offset.y = y;
+	SDL_BlitSurface(source, NULL, destination, &offset);
+}
+
 int main(int argc, char* argv[])
 {
 	SDL_Surface* playerSurface = NULL;
 	SDL_Surface* screen = NULL;
 	SDL_Init(SDL_INIT_EVERYTHING);
 	screen = SDL_SetVideoMode(640, 480, 32, SDL_SWSURFACE);
-	playerSurface = IMG_Load("player.png");
-	SDL_BlitSurface(playerSurface, NULL, screen, NULL);
-	SDL_Flip(screen);
-	SDL_Delay(2000);
+	SDL_WM_SetCaption("The Naked Platformer", NULL);
+	playerSurface = loadImage("player.png");
+	bool quit = false;
+	SDL_Event event;
+	while (!quit)
+	{
+		while (SDL_PollEvent(&event))
+		{
+			if (event.type == SDL_QUIT)
+			{
+				quit = true;
+			}
+		}
+		SDL_FillRect(screen, NULL, SDL_MapRGB(screen->format, 0, 0, 0));
+		applySurface(0, 0, playerSurface, screen);
+		SDL_Flip(screen);
+	}
 	SDL_FreeSurface(playerSurface);
 	SDL_Quit();
 	return 0;
